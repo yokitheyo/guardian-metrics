aab9b13aa178c44392078ded071c8312a73d565c
yokitheyo <razvygs@gmail.com>
2025-06-06 23:08:12 +0300

    added middlewareLogging

--- internal/server/http.go
+++ internal/server/http.go
@@ -5,11 +5,15 @@ import (
 
 	"github.com/gin-gonic/gin"
 	handlerpkg "github.com/yokitheyo/guardian-metrics/internal/server/handlers"
+	"github.com/yokitheyo/guardian-metrics/internal/server/middleware"
 	"github.com/yokitheyo/guardian-metrics/internal/storage"
+	"go.uber.org/zap"
 )
 
-func RunServer(storage storage.Storage, addr string) error {
+func RunServer(storage storage.Storage, addr string, logger *zap.Logger) error {
 	r := gin.Default()
+
+	r.Use(middleware.LoggingMiddleware(logger))
 
 	r.POST("/update/:type/:name/:value", handlerpkg.UpdateMetricHandler(storage))
 	r.GET("/value/:type/:name", handlerpkg.GetMetricValueHandler(storage))
