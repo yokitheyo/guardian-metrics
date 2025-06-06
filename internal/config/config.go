8198b683b0b682b304b8cf31297d7ae4b20379b8
yokitheyo <razvygs@gmail.com>
2025-06-06 20:25:24 +0300

    upd config timeduration

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -19,28 +20,31 @@ type ServerConfig struct {
 
 func LoadAgentConfig() *AgentConfig {
 	conf := &AgentConfig{}
+	var reportInterval, pollInterval int
 
 	flag.StringVar(&conf.Address, "a", "localhost:8080", "address and port to run server")
-	flag.DurationVar(&conf.ReportInterval, "r", 10*time.Second, "report interval")
-	flag.DurationVar(&conf.PollInterval, "p", 2*time.Second, "poll interval")
-
+	flag.IntVar(&reportInterval, "r", 10, "report interval in seconds")
+	flag.IntVar(&pollInterval, "p", 2, "poll interval in seconds")
 	flag.Parse()
+
+	conf.ReportInterval = time.Duration(reportInterval) * time.Second
+	conf.PollInterval = time.Duration(pollInterval) * time.Second
 
 	if envAddr := os.Getenv("ADDRESS"); envAddr != "" {
 		conf.Address = envAddr
 	}
 
 	if envReportInterval := os.Getenv("REPORT_INTERVAL"); envReportInterval != "" {
-		if d, err := time.ParseDuration(envReportInterval + "s"); err == nil {
-			conf.ReportInterval = d
+		if v, err := strconv.Atoi(envReportInterval); err == nil {
+			conf.ReportInterval = time.Duration(v) * time.Second
 		} else {
 			log.Printf("invalid REPORT_INTERVAL: %v, using default", err)
 		}
 	}
 
 	if envPollInterval := os.Getenv("POLL_INTERVAL"); envPollInterval != "" {
-		if d, err := time.ParseDuration(envPollInterval + "s"); err == nil {
-			conf.PollInterval = d
+		if v, err := strconv.Atoi(envPollInterval); err == nil {
+			conf.PollInterval = time.Duration(v) * time.Second
 		} else {
 			log.Printf("invalid POLL_INTERVAL: %v, using default", err)
 		}
